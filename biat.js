2554f82c4c921d0af1ead33bd3da535dfa5dfd1b
bc736 <73925811+bc736@users.noreply.github.com>
2020-11-18 19:59:45 +1100

    Update biat.js

--- biat.js
+++ biat.js
@@ -33,8 +33,32 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/BIAT/qual
 					//Stimulus css (style of the stimuli)
 					stimulusCss : {color:'#31b404','font-size':'2em'}
 				},	
+										
+					name : 'Bullying', //Will appear in the data.
+					title : {
+						media : {word : 'Bullying'}, //Name of the category presented in the task.
+						css : {color:'#31b404','font-size':'1.8em'}, //Style of the category title.
+						height : 4, //Height (because we need to know where to put the next item in the title)
+						startStimulus : { 
+						//If you're using a startStimulus, set here. If not, set the parameter showStimuliWithInst to false (see later below)
+						media : {word : 'Threatening, Degrading, Humiliating, Intimidating, Abusing, Harassing'},
+							css : {color:'#31b404','font-size':'1em'}, 
+							height : 13
+						}
+					}, 
+					stimulusMedia : [ //Stimuli content as PIP's media objects
+				{word : 'Threatening'}, 
+        			{word : 'Degarading'}, 
+        			{word : 'Humiliating'}, 
+        			{word : 'Intimidating'}, 
+        			{word : 'Abusing'},
+				{word : 'Harassing'}	
+						
+					], 
+					//Stimulus css (style of the stimuli)
+					stimulusCss : {color:'#31b404','font-size':'2em'}
+				},	
 						],
-		
 					base_url : {//Where are your images at?
 			image : 'https://baranan.github.io/minno-tasks/images/'
 				
