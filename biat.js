e32c9497cd0d1859377e07c8fee6ec68ef7fd09a
bc736 <73925811+bc736@users.noreply.github.com>
2020-11-18 15:33:16 +1100

    Update biat.js

--- biat.js
+++ biat.js
@@ -50,57 +50,59 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/BIAT/qual
 			},
 			categories : [  //As many categories you need.
 				{
-					name : 'Bullying', //Will appear in the data.
+					name : 'Black People', //Will appear in the data.
 					title : {
-						media : {word : 'Bullying'}, //Name of the category presented in the task.
+						media : {word : 'Black People'}, //Name of the category presented in the task.
 						css : {color:'#31b404','font-size':'1.8em'}, //Style of the category title.
 						height : 4, //Height (because we need to know where to put the next item in the title)
 						startStimulus : { 
 						//If you're using a startStimulus, set here. If not, set the parameter showStimuliWithInst to false (see later below)
-						media : {word : 'Threatening, Degrading, Humiliating, Intimidating, Abusing, Harassing'},
+						media : {image : 'blacks.jpg'}, 
 							css : {color:'#31b404','font-size':'1em'}, 
 							height : 13
 						}
 					}, 
 					stimulusMedia : [ //Stimuli content as PIP's media objects
-				{word : 'Threatening'},
-        			{word : 'Threatening'}, 
-        			{word : 'Degarading'}, 
-        			{word : 'Humiliating'}, 
-        			{word : 'Intimidating'}, 
-        			{word : 'Abusing'},
-				{word : 'Harassing'}
+					{image : 'black1.jpg'}, 
+        			{image : 'black2.jpg'}, 
+        			{image : 'black3.jpg'}, 
+        			{image : 'black4.jpg'}, 
+        			{image : 'black5.jpg'}, 
+        			{image : 'black6.jpg'}
 					], 
 					//Stimulus css (style of the stimuli)
 					stimulusCss : {color:'#31b404','font-size':'2em'}
 				},	
 				{
-					name : 'Bullying', 
+					name : 'White people', 
 					title : {
-						media : {word : 'Bullying'}, 
+						media : {word : 'White people'}, 
 						css : {color:'#31b404','font-size':'1.8em'}, 
 						height : 4,
 						startStimulus : {
-							media : {word : 'Threatening, Degrading, Humiliating, Intimidating, Abusing, Harassing'},
+							media : {image : 'whites.jpg'},  
 							css : {color:'#31b404','font-size':'1em'}, 
 							height : 13
 						}
 					}, 
 					stimulusMedia : [ //Stimuli content as PIP's media objects
-				{word : 'Threatening'},
-        			{word : 'Threatening'}, 
-        			{word : 'Degarading'}, 
-        			{word : 'Humiliating'}, 
-        			{word : 'Intimidating'}, 
-        			{word : 'Abusing'},
-				{word : 'Harassing'}	
+					{image : 'yf1.jpg'}, 
+        			{image : 'yf4.jpg'}, 
+        			{image : 'yf5.jpg'}, 
+        			{image : 'ym2.jpg'}, 
+        			{image : 'ym3.jpg'}, 
+        			{image : 'ym5.jpg'}
 					], 
 					//Stimulus css
 					stimulusCss : {color:'#31b404','font-size':'2em'}
 				}
 			],
-							
-				} 
+			base_url : {//Where are your images at?
+			image : 'https://baranan.github.io/minno-tasks/images/'
+				
+			} 
 	});
 });
+
+
 
