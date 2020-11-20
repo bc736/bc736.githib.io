1cd742128a5988a5ac60828e2e7a312e115db2f0
bc736 <73925811+bc736@users.noreply.github.com>
2020-11-20 21:00:02 +1100

    Update biat.js

--- biat.js
+++ biat.js
@@ -3,48 +3,7 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/BIAT/qual
 
 	
 	return iatExtension({
-		
-		attribute1 : 
-			{
-			name : 'Unpleasant', //Attribute label
-			title : {
-				media : {word : 'Negative'}, //Name of the category presented in the task.
-				css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
-				height : 7 //Used to position the "Or" in the combined block.
-			}, 
-			media : [ //Stimuli
-				{word: 'Bomb'},
-				{word: 'Abuse'},
-				{word: 'Sadness'},
-				{word: 'Pain'},
-				{word: 'Poison'},
-				{word: 'Grief'}
-			], 
-			//Can change color and size of the targets here.
-			css : {color:'#31b404','font-size':'3em'}
-			},
-	
-		attribute2 : 
-			{
-			name : 'Pleasant', //Attribute label
-			title : {
-				media : {word : 'Positive'}, //Name of the category presented in the task.
-				css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
-				height : 7 //Used to position the "Or" in the combined block.
-			}, 
-			media : [ //Stimuli
-				{word: 'Paradise'},
-				{word: 'Pleasure'},
-				{word: 'Cheer'},
-				{word: 'Wonderful'},
-				{word: 'Splendid'},
-				{word: 'Love'}
-			], 
-			//Can change color and size of the targets here.
-			css : {color:'#31b404','font-size':'3em'}
-			},
-		
-		
+				
 		nCategoryAttributeBlocks : 1, 
 		
 		practiceCategory1 : 
@@ -146,6 +105,49 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/BIAT/qual
 			image : 'https://baranan.github.io/minno-tasks/images/'
 				
 			} 
+		
+		
+		attribute1 : 
+			{
+			name : 'Unpleasant', //Attribute label
+			title : {
+				media : {word : 'Negative'}, //Name of the category presented in the task.
+				css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
+				height : 7 //Used to position the "Or" in the combined block.
+			}, 
+			media : [ //Stimuli
+				{word: 'Bomb'},
+				{word: 'Abuse'},
+				{word: 'Sadness'},
+				{word: 'Pain'},
+				{word: 'Poison'},
+				{word: 'Grief'}
+			], 
+			//Can change color and size of the targets here.
+			css : {color:'#31b404','font-size':'3em'}
+			},
+	
+		attribute2 : 
+			{
+			name : 'Pleasant', //Attribute label
+			title : {
+				media : {word : 'Positive'}, //Name of the category presented in the task.
+				css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
+				height : 7 //Used to position the "Or" in the combined block.
+			}, 
+			media : [ //Stimuli
+				{word: 'Paradise'},
+				{word: 'Pleasure'},
+				{word: 'Cheer'},
+				{word: 'Wonderful'},
+				{word: 'Splendid'},
+				{word: 'Love'}
+			], 
+			//Can change color and size of the targets here.
+			css : {color:'#31b404','font-size':'3em'}
+			},
+		
+		
 	});
 });
 
