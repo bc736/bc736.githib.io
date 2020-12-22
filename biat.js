bf89d1942b32c2c8caf4d5ece42334ff14e0bfa0
bc736 <73925811+bc736@users.noreply.github.com>
2020-12-22 19:23:15 +1100

    Update biat.js

--- biat.js
+++ biat.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://bc736.github.io/biat/qbiat.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/BIAT/qualtrics/qbiat6.js'], function(APIConstructor, iatExtension){
  var API = new APIConstructor();
 		
 	return iatExtension({
