b2b8d362c7f37b61585e12dc45b11e27506fb919
bc736 <73925811+bc736@users.noreply.github.com>
2020-12-27 13:27:53 +1100

    Update and rename biat.js to biat_bull.js

--- biat_bull.js
+++ biat_bull.js
@@ -5,7 +5,7 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/BIAT/qual
 		
 		nCategoryAttributeBlocks : 1, 
 		
-		isTouch: true, //Set whether the task is on a touch device.
+		isTouch: false, //Set whether the task is on a touch device.
 		attribute1 : 
 			{
 				name : 'Like', 
