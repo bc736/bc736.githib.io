dcf75928343ce11bffee8728ab7e797ad35e03e4
bc736 <73925811+bc736@users.noreply.github.com>
2020-12-31 11:07:35 +1100

    Update biat_bull.js

--- biat_bull.js
+++ biat_bull.js
@@ -6,9 +6,6 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/BIAT/biat
 		
 		nCategoryAttributeBlocks : 1, 
 		
-		minRT : 0, //Not below this latency
-		maxRT : 10000, //Not above this
-		
 		
 		practiceBlock : false, 
 		
