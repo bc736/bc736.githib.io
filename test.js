39794371877e7fbf4620932637a2336b3869b3d8
bc736 <73925811+bc736@users.noreply.github.com>
2022-12-22 16:22:36 +1100

    Update test.js

--- test.js
+++ test.js
@@ -9,10 +9,8 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/stiat/qua
 					block : 1, //The block variable is not used later, but could help the user. 
 					//In each block, we can include a number of mini-blocks, to reduce repetition of same group/response.
 					miniBlocks : 1, //Set to 1 if don't need mini blocks. 0 will break the task.
-					singleAttTrials : 10, //Number of trials of the attribute that does not share key with the category (in a mini block).
-					sharedAttTrials : 10, //Number of trials of the attribute that shares key with the category (in a mini block).
-					categoryTrials : 0 // Number of trials of the category (in a mini-block). If 0, the label does not appear.
-					//Note: if no category trials, then attribute1, the one on the left, is considered the single attribute.
+			
+			
 				}, 
 				{ 
 					instHTML : '', 
